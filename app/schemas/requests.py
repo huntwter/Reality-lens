cb5aaa5c9eeb811d0c84fecc029efd66f1973883
Huntwter <195620992+balloonflame7@users.noreply.github.com>
2026-01-27 22:01:48 +0530

    chore: Remove all source code, repository now documentation-only

--- app/schemas/requests.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class AnalysisRequest(BaseModel):
-    """
-    Schema for incoming user requests.
-    """
-    query: str
-    context: Optional[str] = None
--- wsgi.py
+++
@@ -1,4 +0,0 @@
-from main import app
-
-# PythonAnywhere looks for an 'application' object by default in the WSGI file
-application = app
